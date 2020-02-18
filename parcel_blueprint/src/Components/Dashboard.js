3415c9316f05eb03b6e6114ed8382cd5640dc5e6
jjacob151 <jayne_jacobs@cable.comcast.com>
2020-02-18 07:34:14 -0500

    Hackman Readme.md dist fonts fonts.zip hackman.github.io-master hackman.github.io-master.zip node_modules package-lock.json package.json src

--- parcel_blueprint/src/Components/Dashboard.js
+++ parcel_blueprint/src/Components/Dashboard.js
@@ -3,11 +3,21 @@ import styled from 'styled-components';
 import { AppContext } from './AppContext.js'
 
 
-const StyledDashboard = styled.Div`
+const StyledDashboard = styled.div`
+    position: fixed;
+    z-index: 2;
+    width: 100 visitWithTypeInfo;
+    top: 0px;
 
+    height: 6.5rem;
 
+    box-shadow: 0 0 1px 1px rgba(20,23,28,.1),0 3px 1px rgba(20,23,28,.1);
+    font-size: 2rem
+    color: #505763;
+    background: #000;
+
+    font-family: "Hackman-Bold';
 `;
-
 
 import ReactJson from 'react-json-view';
 
--- parcel_blueprint/src/Components/NavBar.js
+++ parcel_blueprint/src/Components/NavBar.js
@@ -2,21 +2,7 @@ import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { AppContext } from './AppContext.js'
 
-const StyledDashboard = styled.div`
-    position: fixed;
-    z-index: 2;
-    width: 100 visitWithTypeInfo;
-    top: 0px;
 
-    height: 6.5rem;
-
-    box-shadow: 0 0 1px 1px rgba(20,23,28,.1),0 3px 1px rgba(20,23,28,.1);
-    font-size: 2rem
-    color: #505763;
-    background:L #fff;
-
-    font-family: "Hackman-Bbold';
-`;
 
 import ReactJson from react-json-view;
 import { visitWithTypeInfo } from 'graphql';
